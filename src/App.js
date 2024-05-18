d5cbc28904eb7a2a62ac19dab0161aacbdfb31e6
Rohan Joseph Mathew <56034964+ron-matt163@users.noreply.github.com>
2024-05-18 12:58:27 -0400

    Added state to internally store the step number

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,23 +6,37 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 2;
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    setStep(step <= 1 ? step : step - 1);
+  }
+
+  function handleNext() {
+    setStep(step >= 3 ? step : step + 1);
+  }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">
         Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
